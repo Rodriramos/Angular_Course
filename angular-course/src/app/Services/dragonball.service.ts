44f0657d8c0b22b2c00757147d31023489072173
Rodrigo <ramoshdezrodrigo@gmail.com>
2025-11-10 10:04:23 +0000

    Save in local storage

--- angular-course/src/app/Services/dragonball.service.ts
+++ angular-course/src/app/Services/dragonball.service.ts
@@ -1,14 +1,18 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({ providedIn: 'root' })
 export class DragonballService {
   characters = signal<Character[]>([
-    {id: 1, name: 'Goku', power: 9001},
-    {id: 2, name: 'Vegetta', power: 8000},
-    {id: 3, name: 'Piccolo', power: 3000},
-    {id: 4, name: 'Yamcha', power: 500}
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegetta', power: 8000 },
+    { id: 3, name: 'Piccolo', power: 3000 },
+    { id: 4, name: 'Yamcha', power: 500 }
   ])
+
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  });
 
   addCharacter(newCharacter: Character) {
     this.characters.update(characters => [...characters, newCharacter]);
