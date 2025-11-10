0dae94150c95ccd58755d7391355a7d19886d5d9
Rodrigo <ramoshdezrodrigo@gmail.com>
2025-11-10 15:01:15 +0000

    Designing search 4:57

--- gifs-app/src/app/gifs/pages/search-page/search-page.ts
+++ gifs-app/src/app/gifs/pages/search-page/search-page.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GifList } from "../../components/gif-list/gif-list";
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifList],
   templateUrl: './search-page.html',
   styleUrl: './search-page.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchPage { }
+export default class SearchPage {
+  onSearch(query: string) {
+    console.log({ query });
+  }
+}
